7daf1b092ac4d5df7d5dd32e69acf47e043a489f
Satyam Ghodasara <sghodas@gmail.com>
2014-06-06 00:40:28 -0400

    Changed command to tesseract

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ var PORT = Number(process.env.PORT || 5000);
 var app = express();
 app.use(logfmt.requestLogger());
 
-var child = exec('tesseract -v', function(err, stdout, stderr) {
-    console.log('stdout: ' + stdout);
+var child = exec('tesseract', function(err, stdout, stderr) {
+    console.log('stdout: ' + stdout + '\nstderr: ' + stderr + '\nerr: ' + err);
 });
 
 //    Routes
