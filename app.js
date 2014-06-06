5b95ab4db6ef678ded9a5c038feb406564da63cc
Satyam Ghodasara <sghodas@gmail.com>
2014-06-06 00:05:33 -0400

    Added test command

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
+//    Dependencies
 var express = require('express');
 var logfmt = require('logfmt');
+var exec = require('child_process').exec;
 
+//    Setup
 var PORT = Number(process.env.PORT || 5000);
-
 var app = express();
-
 app.use(logfmt.requestLogger());
 
+var child = exec('pwd', function(err, stdout, stderr) {
+    console.log('stdout: ' + stdout);
+});
+
+//    Routes
 app.get('/', function(req, res) {
     res.send('Hello, world!');
 });
