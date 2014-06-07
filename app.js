ed7935fb2a1d0822d8575a14e1b1c84de314138c
Satyam Ghodasara <sghodas@gmail.com>
2014-06-07 18:56:49 -0400

    Specified tesseract path'

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.bodyParser({limit: '50mb'}));
 //    Routes
 app.post(API_PREFIX + '/recognize', function(req, res) {
     fs.writeFileSync('/tmp/test.png', new Buffer(req.body.imageData, 'base64'));
-    exec('tesseract /tmp/test.png /tmp/test hocr', function(err, stdout, stderr) {
+    exec('/app/vendor/tesseract-ocr/bin/tesseract /tmp/test.png /tmp/test hocr', function(err, stdout, stderr) {
         console.log('err: ' + err + '\nstdout: ' + stdout + '\nstderr: ' + stderr);
         var wordBoxes = util.getWordBoxes(fs.readFileSync('/tmp/test.hocr', 'utf8'));
         res.send(wordBoxes);
